91205837509c047fde35b9def90545fe0ca9d07a
Andrei Hodanenok <xoda91@gmail.com>
2020-11-30 06:42:31 +0300

    Add spring boot to the project

--- pokemons-cli/src/main/java/ahodanenok/pokemons/cli/CLI.java
+++ pokemons-cli/src/main/java/ahodanenok/pokemons/cli/CLI.java
@@ -1,17 +1,25 @@
 package ahodanenok.pokemons.cli;
 
 import ahodanenok.pokemons.cli.command.MainCommand;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
+@SpringBootApplication
+@ImportResource({"ahodanenok/pokemons/importer/context.xml"})
 public class CLI {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
-        context.setConfigLocation("ahodanenok/pokemons/cli/context.xml");
-        context.setParent(new ClassPathXmlApplicationContext("ahodanenok/pokemons/core/context.xml", "ahodanenok/pokemons/importer/context.xml"));
-        context.refresh();
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+//        context.setConfigLocation("ahodanenok/pokemons/cli/context.xml");
+//        context.setParent(new ClassPathXmlApplicationContext("ahodanenok/pokemons/core/context.xml", "ahodanenok/pokemons/importer/context.xml"));
+//        context.refresh();
+//
+//        MainCommand main = context.getBean("cliMain", MainCommand.class);
+//        main.execute();
 
-        MainCommand main = context.getBean("cliMain", MainCommand.class);
-        main.execute();
+        ApplicationContext context = SpringApplication.run(CLI.class, args);
+        context.getBean("cliMain", MainCommand.class).execute();
     }
 }
